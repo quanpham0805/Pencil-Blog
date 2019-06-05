e45c5fe03831794369036deaab61689eb9eb8b79
Quan Pham <shiroemon95@gmail.com>
2019-06-04 22:45:43 -0300

    model is almost finished

--- Pages/admin.py
+++ Pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PencilType, PencilManufacturer, Pencil, Comment
 
 # Register your models here.
+
+admin.site.register(PencilType)
+admin.site.register(PencilManufacturer)
+admin.site.register(Pencil)
+admin.site.register(Comment)
--- Pages/models.py
+++ Pages/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
+import pytz
+from django.conf import settings
+timezone.activate(settings.TIME_ZONE)
 
 # Create your models here
 
 class PencilType(models.Model):
     type_name = models.CharField(max_length=100)
-    type_summary = models.CharField(max_length=300)
-    #type_img =
+    type_description = models.CharField(max_length=300)
     type_slug = models.CharField(max_length=200, default=1)
+    #type_img =
 
     def __str__(self):
         return self.type_name
@@ -16,15 +19,39 @@ class PencilManufacturer(models.Model):
     manufacturer_for_type = models.ForeignKey(PencilType, default=1, verbose_name="Type", on_delete=models.SET_DEFAULT)
     manufacturer_name = models.CharField(max_length=100)
     manufacturer_description = models.CharField(max_length=300)
-    manufacturer_img =
+    #manufacturer_img =
 
     def __str__(self):
         return self.manufacturer_name
 
 class Pencil(models.Model):
+    pencil_for_manufacturer = models.ForeignKey(PencilManufacturer, default=1, verbose_name="Pencil", on_delete=models.SET_DEFAULT)
     pencil_name = models.CharField(max_length=200)
     pencil_description = models.TextField()
-    pencil_published = models.DateTimeField('date published')
-    pencil_author =
-    pencil_comment =
-    pencil_img =
+    pencil_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pencil_last_updated = models.DateTimeField(blank=True, null=True)
+    pencil_published_date = models.DateTimeField(blank=True, null=True)
+    pencil_already_published = models.BooleanField(default=False)
+    pencil_slug = models.CharField(max_length=200, default=1)
+    # pencil_img =
+
+    def publish(self):
+    	if self.pencil_already_published == False:
+	        self.pencil_published_date = timezone.localtime(timezone.now())
+	    	self.pencil_last_updated = self.pencil_published_date
+	    	self.pencil_already_published = True
+	    else:
+	    	self.pencil_last_updated = timezone.localtime(timezone.now())
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Pencil, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    comment_published_date = models.DateTimeField(timezone.localtime(timezone.now()))
+
+    def __str__(self):
+        return self.text
