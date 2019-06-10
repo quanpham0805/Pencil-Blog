7afeff9175310d95fab16c029592c136d0db57ce
Quan Pham <shiroemon95@gmail.com>
2019-06-10 11:26:23 -0300

    bug in admin right now

--- Pages/admin.py
+++ Pages/admin.py
@@ -4,16 +4,39 @@ from .models import PencilType, PencilManufacturer, Pencil, Comment
 # Register your models here.
 
 # class PencilTypeAdmin(admin.ModelAdmin):
-
+# 	fieldsets = [
+#         ("Type", {'fields': ["type_name"]}),
+#         ("Description", {'fields': ["type_description"]}),
+#         ("Slug", {'fields': ["type_slug"]}),
+#         ("Image", {"fields": ["type_img"]})
+#     ]
+#     # list_display = ['type_name', 'id', 'type_slug']
 
 # class PencilManugacturerAdmin(admin.ModelAdmin):
-
+#     #ordering = ['manufacturer_name']
+#     #list_display = ['manufacturer_name', 'id']
+# 	fieldsets = [("Manufacturer", {'fields': ["manufacturer_name"]})]
 
 # class PencilAdmin(admin.ModelAdmin):
-
+# 	fieldsets = [
+#         ("Pencil", {'fields': ["pencil_name"]}),
+#         ("Description", {'fields': ["pencil_description"]}),
+#         ("Author", {'fields': ["pencil_author"]}),
+#         ("Published date", {'fields': ["pencil_published_date"]}),
+#         ("Slug", {'fields': ["pencil_slug"]}),
+#         ("Manufacturer", {'fields': ["pencil_for_manufacturer"]}),
+#         ("Image", {"fields": ["pencil_img"]})
+#     ]
+#     # list_display = ['pencil_name', 'pencil_author', 'pencil_slug', 'pencil_for_manufacturer']
 
 # class CommentAdmin(admin.ModelAdmin):
-
+# 	fieldsets = [
+#         ("Post", {'fields': ["comment_post"]}),
+#         ("Author", {'fields': ["comment_author"]}),
+#         ("Content", {'fields': ["comment_text"]}),
+#         ("Published date", {"fields": ["comment_published_date"]})
+#     ]
+#     # list_display = ['comment_post', 'comment_author', 'comment_published_date']
 
 
 # admin.site.register(PencilType, PencilTypeAdmin)
@@ -21,7 +44,15 @@ from .models import PencilType, PencilManufacturer, Pencil, Comment
 # admin.site.register(Pencil, PencilAdmin)
 # admin.site.register(Comment, CommentAdmin)
 
-admin.site.register(PencilType)
-admin.site.register(PencilManufacturer)
-admin.site.register(Pencil)
-admin.site.register(Comment)
+
+class PencilManugacturerAdmin(admin.ModelAdmin):
+    ordering = ['manufacturer_name']
+    list_display = ['manufacturer_name', 'id']
+
+	# fieldsets = [
+	# 	("Manufacturer", {
+	# 		'fields': ["manufacturer_name"]
+	# 	}),
+	# ]
+
+admin.site.register(PencilManufacturer, PencilManugacturerAdmin)
--- Pages/models.py
+++ Pages/models.py
@@ -15,6 +15,9 @@ class PencilType(models.Model):
     def __str__(self):
         return self.type_name
 
+    class Meta:
+        unique_together = ['type_slug']
+
 class PencilManufacturer(models.Model):
     manufacturer_for_type = models.ForeignKey(PencilType, default=1, verbose_name="Type", on_delete=models.SET_DEFAULT)
     manufacturer_name = models.CharField(max_length=100)
@@ -24,17 +27,23 @@ class PencilManufacturer(models.Model):
     def __str__(self):
         return self.manufacturer_name
 
+    class Meta:
+        unique_together = ['manufacturer_name']
+
 class Pencil(models.Model):
     pencil_for_manufacturer = models.ForeignKey(PencilManufacturer, default=1, verbose_name="Pencil", on_delete=models.SET_DEFAULT)
     pencil_name = models.CharField(max_length=200)
     pencil_description = models.TextField()
     pencil_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    pencil_published_date = models.DateTimeField(blank=True, null=True)
+    pencil_published_date = models.DateTimeField(timezone.localtime(timezone.now()))
     pencil_slug = models.CharField(max_length=200, default=1)
     pencil_img = models.ImageField(upload_to='images', blank=True)
 
     def __str__(self):
         return self.pencil_name
+
+    class Meta:
+        unique_together = ['pencil_slug', 'pencil_name']
 
 class Comment(models.Model):
     comment_post = models.ForeignKey(Pencil, on_delete=models.CASCADE)
@@ -43,4 +52,4 @@ class Comment(models.Model):
     comment_published_date = models.DateTimeField(timezone.localtime(timezone.now()))
 
     def __str__(self):
-        return self.comment_text
+        return self.comment_text[:25]
